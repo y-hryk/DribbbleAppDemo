a8d826a17dff684821250de13c873c5f51f354d9
h.yamaguchi <h.yamaguchi@trifort.jp>
2017-11-17 15:46:54 +0900

    プロジェクトファイル追加

---
+++ app/src/main/java/com/example/hyamaguchi/dribbbleappdemo/PagerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hyamaguchi.dribbbleappdemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PagerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pager)
+    }
+}
