e7dadd94b576d19bee0c1815d201890a118461db
h.yamaguchi <h.yamaguchi@h-yamaguchi.local>
2018-03-08 19:19:59 +0900

    詳細画面から戻るボタンタップで戻れるように修正

--- app/src/main/java/com/example/hyamaguchi/dribbbleappdemo/screen/ShotDetailActivity.java
+++ app/src/main/java/com/example/hyamaguchi/dribbbleappdemo/screen/ShotDetailActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
@@ -36,7 +37,17 @@ public class ShotDetailActivity extends AppCompatActivity {
 
 
         if (getSupportActionBar() != null){
+            getSupportActionBar().setHomeButtonEnabled(true);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
